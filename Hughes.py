aba904e547051374213128d52dd3407dc08ea7a4
Jackman3323 <jhughes22@kentdenver.org>
2015-12-01 15:24:42 -0700

    Create Hughes.py

---
+++ Hughes.py
@@ -0,0 +1,9 @@
+def street(x):
+    print: "There are two buildings. One has an adress of 1, and the other, 2. Which building do you want to enter?"
+    x = raw_input()
+    if x == "1":
+        print: "You have chosen the correct building."
+        return
+    else:
+        print: "You did not choose wisely. You fell into a pit of lava. GAME OVER"
+        return
