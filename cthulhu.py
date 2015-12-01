e6d8ebe8bdff0b763caa81a45917bee495b81335
gitwrektkid <ahoff21@kentdenver.org>
2015-12-01 15:15:15 -0700

    Create cthulhu.py

---
+++ cthulhu.py
@@ -0,0 +1,24 @@
+def cthulu():
+    "You see a colossal statue of a giant reptellian beast, with name leafy engraved on a plaque"
+cthulu = (raw_input("Cthulu just wants a friend, will you be his friend?"))
+if cthulu == "yes":
+    print "*Cthulu explodes* You may pass, head north"
+elif cthulu == "no":
+    print "*Cthulu kills you*"
+elif cthulu == "addison":
+    print "congrats you found an easter egg you get absolutely nothing, your welcome!"
+
+elif cthulu == "reptellian"
+    print "leafyishere"
+
+elif cthulu == "leafyishere"
+    print "hisss"
+
+elif cthulu == "hisss"
+    print "illumnati"
+
+elif cthulu == "illumnati"
+    print "there are 3 s' in hisss, 3 x 5 divided 5 is 3, there are 3 sides on a triangle, the illumnati's sign is a triangle = ILLUMNATI CONFIRMED"
+
+else:
+    print "meh, good enough, go north i guess..... I don't know.... just do what your heart tells you."
